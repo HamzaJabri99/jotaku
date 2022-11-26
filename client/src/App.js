971968e8f52c9bbb30084c4a448b05f2f32aef40
unknown <jabri.oldd@gmail.com>
2022-11-26 16:58:38 +0100

    designing navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import "./app.scss";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
-
+import _404 from "./pages/_404/_404";
 const Layout = () => {
   return (
     <div className="app">
@@ -33,6 +34,10 @@ const router = createBrowserRouter([
         element: <Product />,
       },
     ],
+  },
+  {
+    path: "/*",
+    element: <_404 />,
   },
 ]);
 function App() {
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,85 @@
-import React from "react";
-
+import "./navbar.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/imgs/usd.png" alt="" width={"30px"} height={"auto"} />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className="link" to={"/products/1"}>
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to={"/products/2"}>
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to={"/products/3"}>
+              Children
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to={"/products/4"}>
+              Accessories
+            </Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link className="link" to="/">
+            Ｊｏｔａｋｕ
+          </Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to={""}>
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to={""}>
+              About
+            </Link>
+          </div>
+
+          <div className="item">
+            <Link className="link" to={""}>
+              Contact
+            </Link>
+          </div>
+
+          <div className="item">
+            <Link className="link" to={""}>
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span className="">0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
